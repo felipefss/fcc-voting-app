726f25bd51c475383f3e4e18c5c8a017c20133c8
Felipe Santos <felipefss@gmail.com>
2017-03-20 17:49:43 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use('/', express.static(`${__dirname}/src/site`));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
