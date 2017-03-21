645f343ad471332c7f6dea39e9b90632529dd91b
Felipe Santos <felipefss@gmail.com>
2017-03-21 14:24:14 +0100

    Reorder folders.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use('/', express.static(`${__dirname}/src/site`));
+app.use('/', express.static(`${__dirname}/client`));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
